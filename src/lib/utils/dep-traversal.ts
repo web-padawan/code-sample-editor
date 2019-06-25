a32a3add15299fecef27ca6e9623ea0588855421
Elliott Marquez <emarquez@google.com>
2019-06-25 16:34:49 -0700

    dep traversal v2

--- src/lib/utils/dep-traversal.ts
+++ src/lib/utils/dep-traversal.ts
@@ -1,31 +1,32 @@
-import { fetchAsText } from './util';
+import { fetchAsText, fetchAsJSON, endWithSlash } from './util';
+import { TsTypeMap } from './types';
 
 // matches only what's in the url of an import statement.
-const importUrlRegex = /(?:(?:import\s*(?:{?(?:[\s\w,\*]?)+)*}?\s*(?:from)?\s*['"]\s*)(.*)(?:\s*['"]))/g;
-const exportUrlRegex = /(?:(?:export\s*(?:{?(?:[\s\w,\*]?)+)*}?\s*from\s*['"]\s*)(.*)(?:\s*['"]))/g;
+const importUrlRegex = /(?:import\s*(?:{?(?:[\s\w,\*]?)+)*}?\s*(?:from)?\s*['"]\s*)(.*)(?:\s*['"])/g;
+const exportUrlRegex = /(?:export(?:[\s\w,\*])*{?(?:[\s\w,\*])*}?\s*from?\s*['"]\s*)(.*)(?:\s*['"])/g;
 // matches only the import statement not the url
-const importRegex = /(?:(import\s*(?:{?(?:[\s\w,\*]?)+)*}?\s*(?:from)?\s*['"]\s*)(?:.*)(\s*['"]))/g;
-const exportRegex = /(?:(export\s*(?:{?(?:[\s\w,\*]?)+)*}?\s*from\s*['"]\s*)(?:.*)(\s*['"]))/g;
+const importRegex = /(import\s*(?:{?(?:[\s\w,\*]?)+)*}?\s*(?:from)?\s*['"]\s*)(?:.*)(\s*['"])/g;
+const exportRegex = /(export(?:[\s\w,\*])*{?(?:[\s\w,\*])*}?\s*from?\s*['"]\s*)(?:.*)(\s*['"])/g;
 
-const startLineRegex = /^[\/\.]+/;
+const startLineRegex = /^[\.\/]+/;
 
 export const getTsTypesFromDeps = async (
   deps: string[]
-): Promise<Map<string, { body: string; root: string }>> => {
+): Promise<TsTypeMap> => {
   const depsCrawling = [];
-  const fileContentsMap = new Map<string, { body: string; root: string }>();
+  const fileContentsMap = new Map();
   for (const dep of deps) {
     const depCrawled = crawlDepAndUpdateMap(dep, fileContentsMap);
     depsCrawling.push(depCrawled);
   }
 
-  const depCrawled = await Promise.all(depsCrawling);
+  await Promise.all(depsCrawling);
   return fileContentsMap;
 };
 
 const crawlDepAndUpdateMap = async (
   depName: string,
-  fileContentsMap: Map<string, { body: string; root: string }>
+  fileContentsMap: TsTypeMap
 ): Promise<void> => {
   if (!fileContentsMap.has(depName)) {
     const entrypointContents = await getMainTypeDefFromPackageName(depName);
@@ -41,7 +42,7 @@ const crawlDepAndUpdateMap = async (
 
     for (const file of local) {
       if (!fileContentsMap.has(file)) {
-        const fetched = fetchAsText(file).then(res => {
+        const fetched = fetchAsText(`https://unpkg.com/${file}`).then(res => {
           fileContentsMap.set(file, {
             body: res,
             root: entrypointContents.root
@@ -65,43 +66,46 @@ const crawlDepAndUpdateMap = async (
 const getMainTypeDefFromPackageName = async (
   name: string
 ): Promise<{ body: string; root: string } | null> => {
-  const unpkgRoot = `https://unpkg.com/${name}`;
-  const defTypedRoot = `https://unpkg.com/@types/${name}`;
-  const packageJson = await fetch(`${unpkgRoot}/package.json`).then(res =>
-    res.json()
-  );
+  const nameSplit = name.split('/');
+  let pkgName = nameSplit.shift()!;
+  if (pkgName[0] === '@') {
+    const realPkgName = nameSplit.shift()!;
+    pkgName = `${pkgName}/${realPkgName}`;
+  }
+  const fileName = nameSplit.join('/');
+  const unpkgRoot = `https://unpkg.com/${pkgName}`;
+  let main = '';
+  if (fileName) {
+    main = fileName;
+  } else {
+    const packageJson = await fetchAsJSON(`${unpkgRoot}/package.json`);
 
-  const main: string =
-    packageJson.main instanceof String ? packageJson.main : '';
+    main = 'main' in packageJson ? packageJson.main : '';
+  }
 
   if (main) {
     let strippedMain = main.replace(/^\//, '');
-    strippedMain = strippedMain.replace(/\.js/, '');
+    strippedMain = strippedMain.replace(/(\.js)|(\.d\.ts)$/, '');
+    const entryDeclarationUrl = `${unpkgRoot}/${strippedMain}.d.ts`;
 
-    const mainCheck: Promise<string | null> = fetch(
-      `${unpkgRoot}/${strippedMain}.d.ts`
-    ).then(res => {
-      return res.status === 404 ? Promise.resolve(null) : res.text();
-    });
-    const defTypedCheck = fetch(`${defTypedRoot}/${strippedMain}.d.ts`).then(
+    let mainCheck: Promise<string | null> = fetch(entryDeclarationUrl).then(
       res => {
-        return res.status === 404 ? Promise.resolve(null) : res.text();
+        if (res.status === 404) {
+          return Promise.resolve(null);
+        }
+        return res.text();
       }
     );
 
-    const [mainText, defTypedText] = await Promise.all([
-      mainCheck,
-      defTypedCheck
-    ]);
+    const mainText = await mainCheck;
     const lastSlash = strippedMain.lastIndexOf('/');
     const mainRoot = strippedMain.substring(0, lastSlash);
 
     if (mainText) {
-      const root = `${unpkgRoot}/${mainRoot}/`;
+      const root = `${endWithSlash(pkgName)}${
+        mainRoot ? endWithSlash(mainRoot) : ''
+      }`;
       return { body: mainText, root };
-    } else if (defTypedText) {
-      const root = `${defTypedRoot}/${mainRoot}/`;
-      return { body: defTypedText, root };
     }
   }
 
@@ -110,12 +114,18 @@ const getMainTypeDefFromPackageName = async (
 
 const getImportUrlsFromFileContents = (fileContents: string): Set<string> => {
   const urls: Set<string> = new Set();
-  let match: RegExpExecArray | null = null;
-  while ((match = importUrlRegex.exec(fileContents))) {
-    urls.add(match[1]);
+  let match: RegExpExecArray | null = importUrlRegex.exec(fileContents);
+  while (match) {
+    const declarationFile = match[1].replace(/((\.js)|(\.d\.ts))$/, '.d.ts');
+    urls.add(declarationFile);
+    match = importUrlRegex.exec(fileContents);
   }
-  while ((match = exportUrlRegex.exec(fileContents))) {
-    urls.add(match[1]);
+
+  match = exportUrlRegex.exec(fileContents);
+  while (match) {
+    const declarationFile = match[1].replace(/((\.js)|(\.d\.ts))$/, '.d.ts');
+    urls.add(declarationFile);
+    match = exportUrlRegex.exec(fileContents);
   }
   return urls;
 };
@@ -130,9 +140,12 @@ const normalizeUrls = (
   for (const url of urls) {
     const isLocal = url.match(startLineRegex);
     if (isLocal) {
-      const rootedUrlStr = url.replace(startLineRegex, '');
-      const rootedUrlUrl = new URL(`${rootUrl}/${rootedUrlStr}`);
-      const resolvedURL = rootedUrlUrl.href;
+      const rootedUrlStr = url.replace(/^\.\//, '');
+      const fakeDomain = 'https://asdf.com/';
+      const rootedUrlUrl = new URL(
+        `${fakeDomain}${endWithSlash(rootUrl)}${rootedUrlStr}`
+      );
+      const resolvedURL = rootedUrlUrl.href.replace(fakeDomain, '');
       local.add(resolvedURL);
     } else {
       deps.add(url);
